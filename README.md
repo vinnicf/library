f717d5143b7d2311bd531b4a84ef3470150bf47d
Vini Costa <vinnicf@gmail.com>
2022-05-18 13:17:53 -0300

    Create Function to add book to library

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # library
 
 This project aims to design a one page library where the user will be able to add new books and see the ones he has.
+
+
+Pseudocode:
+
+ -  Array to Store Books; DONE
+ -  Constructor for the Book Object; DONE
+ -  Function to Add Book Object to Library (array);
+ -  Form that receives data from user and connect it to the array with the above function;
+ -  List the books on the page;
--- library.js
+++ library.js
@@ -1,6 +1,41 @@
 
 let library = [
-    {book: 'Festim dos Corvos', author: 'George Martin', pages: 570},
-    {book: 'Watchmen', author: 'Alan Moore', pages: 331}
+    {book: 'Festim dos Corvos', author: 'George Martin', pages: 570, read: true},
+    {book: 'Watchmen', author: 'Alan Moore', pages: 331, read:true}
 ]
+
+function Book(book, author, pages, isread) {
+    this.book = book;
+    this.author = author;
+    this.pages = pages;
+    this.read = isread
+}
+
+function addBooktoLibrary (book, author, pages, isread) {
+    const newbook = new Book (book, author, pages, isread)
+    console.log(newbook.author);
+    console.log(newbook);
+
+// Push newbook into the array;
+    library.push(newbook); 
+
+}
+
+const bookgrid = document.getElementsByClassName('bookgrid');
+
+// This receives book item from library (ex library[0]) and build the card on the page 
+const createBookCard = (book) => {
+    const bookcard = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+
+
+
+}
+
 
